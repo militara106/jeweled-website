36769a8c083d05be0be8fddb9b3a20e8c2935873
Alex <militara106@gmail.com>
2021-02-10 15:17:40 -0500

    Intro.js made into stateful, react reveal animation added

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Introduction from '../Components/Introduction';
+import ReactRevealText from "react-reveal-text";
 
-export default function Intro() {
-  return (
-    <Introduction
-          title='Intro'
-          subtitle='wadwadwadwdwadwawdwadawdwadawdwdawd'
+class Intro extends Component {
+  constructor() {
+    super();
+    this.state = { show: false };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, 800);
+  }
+
+  render() {
+    return (
+      <div>
+        <Introduction
+          title={<ReactRevealText show={this.state.show} className="introTitle">Jeweled</ReactRevealText>}
+          subtitle={<div className="introSub"> Transitioning into a more meaningful relationship</div>}
           dark={true}
           id='section1'
         />
-  );
+      </div>
+    );
+  }
 }
+
+export default Intro;
